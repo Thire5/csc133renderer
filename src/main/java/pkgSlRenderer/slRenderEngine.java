d7d3b7c5f1656724943f5e8cb3003e8ca380d195
crhen <crhender8@gmail.com>
2024-10-01 14:45:12 -0700

    added makeContextCurrent, new variables

--- src/main/java/pkgSlRenderer/slRenderEngine.java
+++ src/main/java/pkgSlRenderer/slRenderEngine.java
@@ -1,4 +1,44 @@
 package pkgSlRenderer;
 
+import org.lwjgl.opengl.GL;
+import pkgSlUtils.slWindowManager;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class slRenderEngine {
+    private final int NUM_RGBA = 4;
+    private final int NUM_3D_COORDS = 3;
+    private final int TRIANGLES_PER_CIRCLE = 40;
+    private final float C_RADIUS = 0.05f;
+    private final int MAX_CIRCLES = 100;
+    private final int UPDATE_INTERVAL = 30;
+
+    void render(int win_width, int win_height) {
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        long my_win = glfwCreateWindow(win_width, win_height, "CSC 133", NULL, NULL);
+        if (my_win == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }  //  if (window == NULL)
+        glfwMakeContextCurrent(my_win);
+
+        GL.createCapabilities();
+        float CC_RED = 0.0f, CC_GREEN = 0.0f, CC_BLUE = 1.0f, CC_ALPHA = 1.0f;
+        glClearColor(CC_RED, CC_GREEN, CC_BLUE, CC_ALPHA);
+
+        while (!glfwWindowShouldClose(my_win)) {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(my_win);
+        }
+
+        glfwDestroyWindow(my_win);
+    } // void render()
+
+    public void initOpenGL(slWindowManager window) {
+    }
 }
--- src/main/java/pkgSlUtils/slWindowManager.java
+++ src/main/java/pkgSlUtils/slWindowManager.java
@@ -14,37 +14,13 @@ public class slWindowManager {
         }
     }
     public static slWindowManager get() {
-        slWindowManagerCreate();
-        slWindowManager window = new slWindowManager();
-        return window;
+        return new slWindowManager();
     }
     public void makeContextCurrent() {
         glfwMakeContextCurrent(my_win);
     }
 
-    void render(int win_width, int win_height) {
-            if (!glfwInit()) {
-                throw new IllegalStateException("Unable to initialize GLFW");
-            }
-            long my_win = glfwCreateWindow(win_width, win_height, "CSC 133", NULL, NULL);
-            if (my_win == NULL) {
-                throw new RuntimeException("Failed to create the GLFW window");
-            }  //  if (window == NULL)
-            glfwMakeContextCurrent(my_win);
 
-            GL.createCapabilities();
-            float CC_RED = 0.0f, CC_GREEN = 0.0f, CC_BLUE = 1.0f, CC_ALPHA = 1.0f;
-            glClearColor(CC_RED, CC_GREEN, CC_BLUE, CC_ALPHA);
-
-            while (!glfwWindowShouldClose(my_win)) {
-                glfwPollEvents();
-                glClear(GL_COLOR_BUFFER_BIT);
-
-                glfwSwapBuffers(my_win);
-            }
-
-            glfwDestroyWindow(my_win);
-    } // void render()
 
     public void initGLFWWindow(int winWidth, int winHeight, String title) {
 
