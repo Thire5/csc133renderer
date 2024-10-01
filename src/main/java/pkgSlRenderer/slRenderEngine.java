882199b18e39835929391936aec10cb76782d56b
crhen <crhender8@gmail.com>
2024-10-01 14:47:40 -0700

    moved and reformatted render method

--- src/main/java/pkgSlRenderer/slRenderEngine.java
+++ src/main/java/pkgSlRenderer/slRenderEngine.java
@@ -15,15 +15,13 @@ public class slRenderEngine {
     private final int MAX_CIRCLES = 100;
     private final int UPDATE_INTERVAL = 30;
 
-    void render(int win_width, int win_height) {
+    public void render() {
         if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
-        long my_win = glfwCreateWindow(win_width, win_height, "CSC 133", NULL, NULL);
         if (my_win == NULL) {
             throw new RuntimeException("Failed to create the GLFW window");
         }  //  if (window == NULL)
-        glfwMakeContextCurrent(my_win);
 
         GL.createCapabilities();
         float CC_RED = 0.0f, CC_GREEN = 0.0f, CC_BLUE = 1.0f, CC_ALPHA = 1.0f;
