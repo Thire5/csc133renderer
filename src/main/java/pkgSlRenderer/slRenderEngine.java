f771f289735b28be3dfd967ed06ee217a71d77be
crhen <crhender8@gmail.com>
2024-10-03 16:22:25 -0700

    code cleanup

--- src/main/java/pkgSlRenderer/slRenderEngine.java
+++ src/main/java/pkgSlRenderer/slRenderEngine.java
@@ -8,7 +8,6 @@ import static pkgDriver.slSpot.*;
 import static java.lang.Math.PI;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.NULL;
 public class slRenderEngine {
     private final int NUM_RGBA = 4;
     private final int NUM_3D_COORDS = 3;
@@ -96,11 +95,5 @@ public class slRenderEngine {
         GL.createCapabilities();
         float CC_RED = 0.0f, CC_GREEN = 0.0f, CC_BLUE = 1.0f, CC_ALPHA = 1.0f;
         glClearColor(CC_RED, CC_GREEN, CC_BLUE, CC_ALPHA);
-    }
-    private void checkGLError() {
-        int error = glGetError();
-        if (error != GL_NO_ERROR) {
-            System.err.println("glGetError: " + error);
-        }
     }
 }
