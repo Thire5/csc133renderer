0336f99b628ceeba88a4916f0ea93fcabe91dbed
crhen <crhender8@gmail.com>
2024-10-13 20:45:37 -0700

    added new class, new methods

---
+++ src/main/java/pkgSlPolygonRenderer/slPolygonRenderer.java
@@ -0,0 +1,15 @@
+package pkgSlPolygonRenderer;
+
+import pkgSlRenderer.slRenderEngine;
+
+public class slPolygonRenderer extends slRenderEngine {
+    private void generateVertices(int rows, int cols) {
+
+    }
+    private void generateShapes() {
+
+    }
+    public void render(int frameDelay, int rows, int cols) {
+
+    }
+}
