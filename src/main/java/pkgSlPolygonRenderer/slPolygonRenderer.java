153ec1c51469fe272d1b556fe8f9ed30003fb4e7
crhen <crhender8@gmail.com>
2024-10-14 16:06:29 -0700

    first draft of generateShapes

--- src/main/java/pkgSlPolygonRenderer/slPolygonRenderer.java
+++ src/main/java/pkgSlPolygonRenderer/slPolygonRenderer.java
@@ -1,8 +1,18 @@
 package pkgSlPolygonRenderer;
 
 import pkgSlRenderer.slRenderEngine;
+import pkgSlUtils.slWindowManager;
+
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.opengl.GL11.*;
+import java.util.Random;
+import static pkgDriver.slSpot.*;
+import static java.lang.Math.PI;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 
 public class slPolygonRenderer extends slRenderEngine {
+    private slWindowManager my_wm = new slWindowManager();
     private final int faceMinimum = 3;
     private final int faceMaximum = 20;
     private final int coordsPerVertex = 3;
@@ -10,14 +20,20 @@ public class slPolygonRenderer extends slRenderEngine {
     private final int yCoord = 1;
     private final int zCoord = 2;
     private final float z = 0.0f;
+    private final float windowRange = 2.0f;
     private float[] center = new float[coordsPerVertex];
+    private final int UPDATE_INTERVAL = 200;
+    private final float opacity = 1.0f;
+    private float radius;
+    private float[] vertexOne = new float[coordsPerVertex];
+    private float[] vertexTwo = new float[coordsPerVertex];
+    Random myRand = new Random();
     private void generateVertices(int rows, int cols, int shapeCount) {
-        float radius = 0;
         if(rows >= cols) {
-            radius = (float) (2.0 / (rows * 2));
+            radius = (float) (windowRange / (rows * 2));
         }
-        else if(cols >= rows) {
-            radius = (float) (2.0 / (cols * 2));
+        else if(cols > rows) {
+            radius = (float) (windowRange / (cols * 2));
         }
         center[xCoord] = (-1 + radius);
         center[yCoord] = (1 - radius);
@@ -34,15 +50,55 @@ public class slPolygonRenderer extends slRenderEngine {
         }
     }
     private void generateShapes(int faces) {
-
+        float theta = 0.0f;
+        float thetaInterval = (float) (2 * Math.PI) / faces;
+        float aspectRatio = (float) WIN_HEIGHT / WIN_WIDTH;
+        float xRadius = radius * aspectRatio;
+        vertexOne[xCoord] = (float) (center[xCoord] + (xRadius * Math.cos(theta)));
+        vertexOne[yCoord] = (float) (center[yCoord] + (xRadius * Math.sin(theta)));
+        vertexOne[zCoord] = z;
+        theta += thetaInterval;
+        for(int triangle = 0; triangle < faces; triangle++) {
+            vertexTwo[xCoord] = (float) (center[xCoord] + (xRadius * Math.cos(theta)));
+            vertexTwo[yCoord] = (float) (center[yCoord] + (xRadius * Math.sin(theta)));
+            vertexTwo[zCoord] = z;
+            theta += thetaInterval;
+            glVertex3f(center[xCoord], center[yCoord], center[zCoord]);
+            glVertex3f(vertexOne[xCoord], vertexOne[yCoord], vertexOne[zCoord]);
+            glVertex3f(vertexTwo[xCoord], vertexTwo[yCoord], vertexTwo[zCoord]);
+            vertexOne[xCoord] = vertexTwo[xCoord];
+            vertexOne[yCoord] = vertexTwo[yCoord];
+        }
     }
     public void render(int frameDelay, int rows, int cols) {
         int shapes = rows * cols;
-        for(int faces = faceMinimum; faces <= faceMaximum; faces++) {
-            for (int shape = 0; shape < shapes; shape++) {
-                generateVertices(rows, cols, shape);
-                generateShapes(faces);
+        while (!my_wm.isGlfwWindowClosed()) {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+            //each pass fills window
+            for(int faces = faceMinimum; faces <= faceMaximum; faces++) {
+                glBegin(GL_TRIANGLES);
+                glColor4f(myRand.nextFloat(), myRand.nextFloat(), myRand.nextFloat(), opacity);
+                //each pass creates one polygon
+                for (int shape = 0; shape < shapes; shape++) {
+                    generateVertices(rows, cols, shape);
+                    generateShapes(faces);
+                    glEnd();
+                }
+                my_wm.swapBuffers();
+                if(UPDATE_INTERVAL != 0) {
+                    try {
+                        Thread.sleep(UPDATE_INTERVAL);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
+
+        }
+
+            } // while (!my_wm.isGlfwWindowClosed())
+            //my_wm.destroyGlfwWindow();
         }
     }
 }
