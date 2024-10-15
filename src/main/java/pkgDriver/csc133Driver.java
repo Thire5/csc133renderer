2aa9da25163dcf642ae0b40b3b8a092506a896e3
crhen <crhender8@gmail.com>
2024-10-15 12:11:35 -0700

    driver updates

--- src/main/java/pkgDriver/csc133Driver.java
+++ src/main/java/pkgDriver/csc133Driver.java
@@ -1,12 +1,16 @@
 package pkgDriver;
+import pkgSlPolygonRenderer.slPolygonRenderer;
 import pkgSlRenderer.slRenderEngine;
 import static pkgDriver.slSpot.*;
 import pkgSlUtils.slWindowManager;
 public class csc133Driver {
+    public static final int rows = 30;
+    public static final int cols = 30;
+    public static final int frameDelay = 100;
     public static void main(String[] my_args) {
-        slRenderEngine my_re = new slRenderEngine();
+        slPolygonRenderer my_re = new slPolygonRenderer();
         slWindowManager.get().initGLFWWindow(WIN_WIDTH, WIN_HEIGHT, "CSUS CSC133");
         my_re.initOpenGL(slWindowManager.get());
-        my_re.render();
+        my_re.render(frameDelay, rows, cols);
     } // public static void main(String[] my_args)
 } // public class csc133Driver(...)
--- src/main/java/pkgSlPolygonRenderer/slPolygonRenderer.java
+++ src/main/java/pkgSlPolygonRenderer/slPolygonRenderer.java
@@ -22,33 +22,33 @@ public class slPolygonRenderer extends slRenderEngine {
     private final float z = 0.0f;
     private final float windowRange = 2.0f;
     private float[] center = new float[coordsPerVertex];
-    private final int UPDATE_INTERVAL = 200;
     private final float opacity = 1.0f;
     private float radius;
     private float[] vertexOne = new float[coordsPerVertex];
     private float[] vertexTwo = new float[coordsPerVertex];
     Random myRand = new Random();
+
     private void generateVertices(int rows, int cols, int shapeCount) {
-        if(rows >= cols) {
+        if (rows >= cols) {
             radius = (float) (windowRange / (rows * 2));
-        }
-        else if(cols > rows) {
+        } else if (cols > rows) {
             radius = (float) (windowRange / (cols * 2));
         }
         center[xCoord] = (-1 + radius);
         center[yCoord] = (1 - radius);
         center[zCoord] = (z);
-        if(shapeCount > 0) {
-            while(shapeCount > cols) {
+        if (shapeCount > 0) {
+            while (shapeCount > cols) {
                 center[yCoord] -= (radius * 2);
                 shapeCount -= cols;
             }
-            while(shapeCount > 0) {
+            while (shapeCount > 0) {
                 center[xCoord] += (radius * 2);
                 shapeCount--;
             }
         }
     }
+
     private void generateShapes(int faces) {
         float theta = 0.0f;
         float thetaInterval = (float) (2 * Math.PI) / faces;
@@ -58,7 +58,7 @@ public class slPolygonRenderer extends slRenderEngine {
         vertexOne[yCoord] = (float) (center[yCoord] + (xRadius * Math.sin(theta)));
         vertexOne[zCoord] = z;
         theta += thetaInterval;
-        for(int triangle = 0; triangle < faces; triangle++) {
+        for (int triangle = 0; triangle < faces; triangle++) {
             vertexTwo[xCoord] = (float) (center[xCoord] + (xRadius * Math.cos(theta)));
             vertexTwo[yCoord] = (float) (center[yCoord] + (xRadius * Math.sin(theta)));
             vertexTwo[zCoord] = z;
@@ -70,13 +70,14 @@ public class slPolygonRenderer extends slRenderEngine {
             vertexOne[yCoord] = vertexTwo[yCoord];
         }
     }
+
     public void render(int frameDelay, int rows, int cols) {
         int shapes = rows * cols;
         while (!my_wm.isGlfwWindowClosed()) {
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT);
             //each pass fills window
-            for(int faces = faceMinimum; faces <= faceMaximum; faces++) {
+            for (int faces = faceMinimum; faces <= faceMaximum; faces++) {
                 glBegin(GL_TRIANGLES);
                 glColor4f(myRand.nextFloat(), myRand.nextFloat(), myRand.nextFloat(), opacity);
                 //each pass creates one polygon
@@ -86,19 +87,15 @@ public class slPolygonRenderer extends slRenderEngine {
                     glEnd();
                 }
                 my_wm.swapBuffers();
-                if(UPDATE_INTERVAL != 0) {
+                if (frameDelay != 0) {
                     try {
-                        Thread.sleep(UPDATE_INTERVAL);
+                        Thread.sleep(frameDelay);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                 }
             }
-
-        }
-
-            } // while (!my_wm.isGlfwWindowClosed())
-            //my_wm.destroyGlfwWindow();
-        }
+        } // while (!my_wm.isGlfwWindowClosed())
+        //my_wm.destroyGlfwWindow();
     }
 }
